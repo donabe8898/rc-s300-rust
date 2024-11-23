1513ad5feab8e0c2b3a3b04fc360265222354cd3
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2024-11-23 21:31:31 +0900

    [Add]cargo.toml and crate defined
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use core::slice;
+use dialoguer::Select;
+use hex_literal::hex;
+use pcsc::{
+    Context, Error, Protocols, ReaderNames, ReaderState, Scope, ShareMode, State, MAX_ATR_SIZE,
+    MAX_BUFFER_SIZE, PNP_NOTIFICATION,
+};
+use pcsc_sys::SCardControl;
+use std::ffi::CStr;
+use std::time::Duration;
+use std::{ffi::CString, fmt::format, result};
+use tokio::io;
+
 fn main() {
-    println!("Hello, world!");
+    //  Read Without Encriptの領域を全て吸い出してダンプ
 }
